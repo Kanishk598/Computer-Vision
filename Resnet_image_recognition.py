0b300db8a90318329aa7a77f6850d9e1e60b1621
Kanishk Vishwakarma <52484751+Kanishk598@users.noreply.github.com>
2021-01-29 18:25:47 +0530

    Corrected paths

--- Resnet_image_recognition.py
+++ Resnet_image_recognition.py
@@ -10,10 +10,7 @@ resnet = models.resnet101(pretrained=True)
 #resnet 
 preprocess = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-
-# %%
-from PIL import Image
-img = Image.open('/home/kanishk/Desktop/Small Dataset/14.jpeg') # Image name is '1.jpeg'. Change it from 1.jpeg to 20.jpeg if you want.
+Small Dataset/14.jpeg') # Image name is '1.jpeg'. Change it from 1.jpeg to 20.jpeg if you want.
 img.show()
 
 
@@ -27,7 +24,7 @@ batch_t = torch.unsqueeze(img_t, 0)
 resnet.eval()
 out = resnet(batch_t)
 #out
-with open('/home/kanishk/Desktop/dlwpt-code/data/p1ch2/imagenet_classes.txt') as f:
+with open('imagenet_classes.txt') as f:
     labels = [line.strip() for line in f.readlines()]
 _, index = torch.max(out, 1)
 percentage = 100*torch.nn.functional.softmax(out, dim=1)[0]
