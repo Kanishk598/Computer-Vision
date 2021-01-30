1c99f99e34b555e7d8af218a4c4157328057b350
Kanishk Vishwakarma <52484751+Kanishk598@users.noreply.github.com>
2021-01-30 16:29:07 +0530

    Corrected error in code.

--- Resnet_image_recognition.py
+++ Resnet_image_recognition.py
@@ -10,7 +10,9 @@ resnet = models.resnet101(pretrained=True)
 #resnet 
 preprocess = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-Small Dataset/14.jpeg') # Image name is '1.jpeg'. Change it from 1.jpeg to 20.jpeg if you want.
+# %%
+from PIL import Image
+img = Image.open('Small Dataset/1.jpeg') # Image name is '1.jpeg'. Change it from 1.jpeg to 20.jpeg if you want.
 img.show()
 
 
